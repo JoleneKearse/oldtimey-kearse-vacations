fb97d75c83d4dea97fc5cb95b48181339052cdd6
JoleneKearse <pharaohnutz@gmail.com>
2025-11-29 18:04:22 -0500

    chore: scaffold project

---
+++ app/gallery/page.tsx
@@ -0,0 +1,18 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrev,
+} from "@/components/ui/carousel";
+
+export default function Gallery() {
+  return (
+    <Carousel className="w-full max-w-">
+      <CarouselContent>
+        
+      </CarouselContent>
+    </Carousel>
+  );
+}
---
+++ components/Button.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Noto_Sans } from "next/font/google";
+
+const notoSans = Noto_Sans({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+type ButtonProps = {
+    tooltip: React.ReactNode
+    children: React.ReactNode
+}
+
+const Button = ({ tooltip, children }: ButtonProps) => {
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <button className="cursor-pointer">
+         {children}
+        </button>
+      </TooltipTrigger>
+      <TooltipContent className={`${notoSans.className} font-bold text-lg`}>{tooltip}</TooltipContent>
+    </Tooltip>
+  );
+};
+
+export default Button;
