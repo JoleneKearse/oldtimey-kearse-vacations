2772bbc1bd4d37053f3000d710f595626523412d
JoleneKearse <pharaohnutz@gmail.com>
2025-12-02 10:08:25 -0500

    feat: stub out gallery page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,18 +1,23 @@
-import { Card, CardContent } from "@/components/ui/card";
+"use client";
+
+// import { Card, CardContent } from "@/components/ui/card";
+import { Image } from "next/image";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
-  CarouselPrev,
+  CarouselPrevious,
 } from "@/components/ui/carousel";
 
 export default function Gallery() {
   return (
     <Carousel className="w-full max-w-">
       <CarouselContent>
-        
+        <CarouselItem key={index}></CarouselItem>
       </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
     </Carousel>
   );
 }
