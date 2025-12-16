577f5b4f68a04b95de4a1f8eb1ce0081ed06d824
JoleneKearse <pharaohnutz@gmail.com>
2025-12-16 10:52:49 -0500

    chore: add suspense boundary to not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,18 @@
+import { Suspense } from "react";
 import Link from "next/link";
 
 const NotFound = () => {
-    return ( <article>
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <article>
         <h1>Something went wrong</h1>
         <p>The dogs probably did it</p>
-        <Link href="/" className="text-blue-500 underline">Go back home</Link>
-    </article> );
-}
- 
+        <Link href="/" className="text-blue-500 underline">
+          Go back home
+        </Link>
+      </article>
+    </Suspense>
+  );
+};
+
 export default NotFound;
